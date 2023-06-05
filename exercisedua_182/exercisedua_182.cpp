e184da3cfcfeda9e266020aff66f6b587e9dd59c
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-06-05 14:08:08 +0700

    p

--- exercisedua_182/exercisedua_182.cpp
+++ exercisedua_182/exercisedua_182.cpp
@@ -38,7 +38,7 @@ void pengarang::tambahPenerbit(penerbit* pPenerbit) {
 }
 
 void pengarang::cetakPenerbit() {
-	cout << "Daftar penerbit yang menerbitkan buku pengarang \"" << this->nama << "\":\n";
+	cout << "Daftar penerbit yang menerbitkan buku \"" << this->nama << "\":\n";
 	for (auto& a : daftar_penerbit) {
 		cout << a->nama << "\n";
 	}
@@ -51,9 +51,40 @@ void penerbit::tambahPengarang(pengarang* pPengarang) {
 }
 
 void penerbit::cetakPengarang() {
-	cout << "Daftar buku Pengarang yang diterbitkan \"" << this->nama << "\":\n";
+	cout << "Daftar buku yang diterbitkan \"" << this->nama << "\":\n";
 	for (auto& a : daftar_pengarang) {
 		cout << a->nama << "\n";
 	}
 	cout << endl;
+}
+
+int main() {
+	penerbit* varPenerbit1 = new penerbit("Gama Press");
+	penerbit* varPenerbit2 = new penerbit("Intan Pariwara");
+	pengarang* varPengarang1 = new pengarang("Giga");
+	pengarang* varPengarang2 = new pengarang("Asroni");
+	pengarang* varPengarang3 = new pengarang("Lia");
+	pengarang* varPengarang4 = new pengarang("Joko");
+
+	varPenerbit1->tambahPengarang(varPengarang4);
+	varPenerbit1->tambahPengarang(varPengarang3);
+	varPenerbit1->tambahPengarang(varPengarang1);
+	varPenerbit2->tambahPengarang(varPengarang2);
+	varPenerbit2->tambahPengarang(varPengarang1);
+
+	varPenerbit1->cetakPengarang();
+	varPenerbit2->cetakPengarang();
+	varPengarang1->cetakPenerbit();
+	varPengarang2->cetakPenerbit(); 
+	varPengarang3->cetakPenerbit(); 
+	varPengarang4->cetakPenerbit();
+
+	delete varPengarang1;
+	delete varPengarang2; 
+	delete varPengarang3; 
+	delete varPengarang4;  
+	delete varPenerbit1;
+	delete varPenerbit2;
+
+	return 0;
 }
